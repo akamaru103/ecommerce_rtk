04b40202fda9f55aac7d5d23e81fc38f2d219157
akamaru103 <shibainu103@gmail.com>
2025-05-27 04:12:03 -0400

    Completed tutorial

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice'; // Action to add product to cart
 import './ProductList.css'; 
 
-const ProductList = () => {
+
+
+const ProductList = () => {    
+
+    const dispatch = useDispatch();
+
+    const cartItems = useSelector(state => state.cart.cartItems); // Get cart items globally
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
@@ -9,11 +17,29 @@ const ProductList = () => {
     { id: 3, name: 'Product C', price: 30 },
   ];
 
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));// Add product to cart
+  };
+
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+        {products.map((product) => {
+          const isAlreadyInCart = cartItems.some(item => item.id === product.id); // Check if product is already in cart
+          return (
+            <li key={product.id} className="product-list-item">
+              <span>{product.name} - ${product.price}</span>
+              <button
+                className={`add-to-cart-btn ${isAlreadyInCart ? 'disabled' : ''}`}
+                onClick={() => handleAddToCart(product)}
+                disabled={isAlreadyInCart} // Disable button if already added
+              >
+                {isAlreadyInCart ? 'Added' : 'Add to Cart'}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
